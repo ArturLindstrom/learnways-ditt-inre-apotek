c3d4ecc416a1d2f2a66137724dc5104f9d4f4421
Artur Lindström <artur.lindstrom@gmail.com>
2022-09-29 12:57:10 +0200

    asdf

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <NavComponent/>
-  <RouterView />
+  
+  <LoaderComponent id="loader" v-show="!pageLoaded"/>
+  <div class="app" v-cloak>
+    <NavComponent />
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
 import { RouterView } from 'vue-router'
 import NavComponent from './components/NavComponent.vue';
+import LoaderComponent from './components/LoaderComponent.vue';
+
+const pageLoaded = ref(false)
+
+document.onreadystatechange = function() {
+            if (document.readyState !== "complete") {
+                // document.querySelector(
+                //   ".app").style.visibility = "hidden";
+                // document.querySelector(
+                //   "#loader").style.visibility = "visible";
+            } else {
+                // document.querySelector(
+                //   "#loader").style.display = "none";
+                  pageLoaded.value = true
+                // document.querySelector(
+                //   ".app").style.visibility = "visible";
+            }
+        };
+
 </script>
 
 
 <style lang="scss">
+[v-cloak]{
+  display: none;
+}
+
 /* #app{
   width: 100%;
 } */
---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <h1>HALLÅ
+    
+  </h1>
+  <!-- <img src="/assets/img/apelsin-inre-apotek.png" alt="" width="100px"> -->
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang='scss'>
+  div {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+  }
+</style>
