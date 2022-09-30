15caad2bf298faff0dd50c0a50fc759407cfe48b
Artur Lindström <84621455+ArturLindstrom@users.noreply.github.com>
2022-09-30 09:21:39 +0200

    fixed broken links in nav

--- src/components/DialogComponent.vue
+++ src/components/DialogComponent.vue
@@ -76,7 +76,7 @@ const toggleDialog = (i) => {
 .dialog-wrapper {
   position: relative;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   flex-direction: column;
   width: 90%;
